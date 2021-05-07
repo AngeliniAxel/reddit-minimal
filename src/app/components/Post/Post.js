c0b28c2dd3ec7ea97e04a103ad74b9587a94bc88
AngeliniAxel <axel.angelini@gmail.com>
2021-05-07 18:29:56 -0300

    Adding style to Post

--- src/app/components/Post/Post.js
+++ src/app/components/Post/Post.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComments } from '@fortawesome/free-solid-svg-icons';
+
+import './Post.scss';
+
 const Post = ({ post }) => {
   const {
     title,
@@ -19,20 +24,20 @@ const Post = ({ post }) => {
   return (
     <div>
       {post_hint == 'image' && (
-        <div>
-          <h3>title: {title}</h3>
-          <h2>author: {author}</h2>
-          <h3>subreddit: {subreddit}</h3>
-          <img src={url} height='200' width='200' alt='' />
-          <h3>{url}</h3>
-          <h3>post_hint: {post_hint}</h3>
-          <h3>is_video: {is_video ? 'yes' : 'no'}</h3>
-          <h3>permalink: {permalink}</h3>
-          <h3>thumbnailUrl: {thumbnailUrl}</h3>
-          <h3>id: {id}</h3>
-          <h3>ups: {ups}</h3>
-          <h3>created_utc: {created_utc}</h3>
-          <h3>num_comments {num_comments}</h3>
+        <div className='responsive'>
+          <h3>{title}</h3>
+          <div className='imgContainer'>
+            <img src={url} alt='image from user' />
+          </div>
+          <div className='info'>
+            <h4>
+              <span className='detail'>Posted by</span> {author}
+            </h4>
+            <h4>
+              <FontAwesomeIcon icon={faComments} size='2x' />
+              <span className='comments'>{num_comments}</span>
+            </h4>
+          </div>
         </div>
       )}
     </div>
