75335aee9c55ea80617f8f49384e3a15dd73b44c
AngeliniAxel <axel.angelini@gmail.com>
2021-05-16 21:06:28 -0300

    changes for netlify

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import {
